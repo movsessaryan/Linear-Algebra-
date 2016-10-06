cb61526d3167a1d6c51eab2c7bf9d7482852e1b8
movsessaryan <movsessaryan@gmail.com>
2016-10-06 16:10:22 +0400

    Add files via upload

--- HomeWorke.py
+++ HomeWorke.py
@@ -1,55 +1,52 @@
-from fractions import Fraction
-fr = Fraction
-fr.__repr__ = fr.__str__
-''' Gaus's algorithm '''
-def norm(matrix , ind):
-    for i in range(ind+1,len(matrix)):
-        if matrix[i][ind] != 0:
-            matrix[ind],matrix[i] = matrix[i],matrix[ind]
-def z(matrix, ind):
-    for i in range(ind+1,len(matrix)):
-        alpha = matrix[i][ind]/matrix[ind][ind]
-        for j in range(len(matrix[0])):
-            matrix[i][j] -= alpha*matrix[ind][j]
 
-def gaus(matrix,f=z):
+''' Gaus's algorithm '''
+def norm(matrix, ind):
+    for i in range(ind + 1, len(matrix)):
+        if matrix[i][ind] != 0:
+            matrix[ind], matrix[i] = matrix[i], matrix[ind]
+
+
+def z(matrix, ind):
+    for i in range(ind + 1, len(matrix)):
+        b = matrix[i][ind]
+        a = matrix[ind][ind]
+        g = gcd(a,b)
+        for j in range(len(matrix[0])):
+            matrix[i][j] = matrix[i][j]*(a//g) - matrix[ind][j] * (b//g)
+
+
+def gaus(matrix, f=z):
     n = min(len(matrix), len(matrix[0]))
     for i in range(n):
-        norm(matrix,i)
-        f(matrix,i)
+        norm(matrix, i)
+        f(matrix, i)
 
 
 def normalize(row):
-    m = 1    
-    for item in row:
-        m *= item.denominator
-
-    for i in range(len(row)):
-        row[i] *= m
-
-    t = [x.numerator for x in row]
-    g = gcd_aray(t)
-    if g == 0:
+    g = gcd_array(row)
+    if g == 0 or g == 1:
         return
     for i in range(len(row)):
         row[i] /= g
 
-def gcd_aray(arr):
+
+def gcd_array(arr):
     def temp(start):
-        if start == len(arr) -1:
-            return arr[len(arr) -1]
-        return gcd(arr[start],temp(start+1))
+        if start == len(arr) - 1:
+            return arr[len(arr) - 1]
+        return gcd(arr[start], temp(start + 1))
+
     return temp(0)
 
-def gcd(a,b):
+
+def gcd(a, b):
     if b == 0:
         return a
-    return gcd(b,a%b)
+    return gcd(b, a % b)
 
-
-matrix = [[fr(12),fr(13),fr(12)],[fr(1),fr(123),fr(0)],[fr(12),fr(23),fr(3)], [fr(12),fr(23),fr(3)], [fr(10),fr(22),fr(31)]]
+matrix = [[12,13,12],[1,123,0],[12,23,3], [12,23,3], [10,22,31]]
 gaus(matrix)
 for item in matrix:
     normalize(item)
     print (item)
- 
+
